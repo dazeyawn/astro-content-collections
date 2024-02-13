ba1bea54e0686153944e6cc55089d0bc0c4e5724
dazeyawn <3rciek86kn@gmail.com>
2024-02-14 02:20:54 +0900

    initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from "astro:content";
+
+const postCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()),
+      image: image(),
+    }),
+});
+
+export const collections = {
+  post: postCollection,
+};
