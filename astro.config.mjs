e81c85e3a6eb400b6ca8710bd0965b9db3531c0c
dazeyawn <3rciek86kn@gmail.com>
2024-02-14 04:31:42 +0900

    🐛 00x1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dazeyawn-astro-content-collections.netlify.app/",
   integrations: [mdx()],
   image: {
     domains: ["astro.build"],
